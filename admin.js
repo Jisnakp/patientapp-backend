e18372d9e2f0cabcc28521c9b4be7ea791a91423
Jisnakp <jisnab01@gmail.com>
2024-08-01 14:50:37 +0530

    created

---
+++ admin.js
@@ -0,0 +1,9 @@
+const mongoose=require("mongoose")
+const LoginSchema=mongoose.Schema(
+    {
+        username:String,
+        password:String
+    }
+)
+const loginModel=mongoose.model("logindata",LoginSchema)
+module.exports=loginModel
